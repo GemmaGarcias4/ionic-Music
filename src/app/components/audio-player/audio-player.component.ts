dc74d54e95eb2ceb0773176762d0a9b868e207a6
GemmaGarcias4 <gemmagarcias@xpertai.com>
2020-03-23 04:54:03 +0100

    get duration

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { Component, OnChanges, Input, ViewChild, ElementRef, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-audio-player',
   templateUrl: './audio-player.component.html',
   styleUrls: ['./audio-player.component.scss'],
 })
-export class AudioPlayerComponent implements OnChanges {
+export class AudioPlayerComponent implements OnChanges, AfterViewInit {
 
   @Input() audio: {url: string, loop: boolean, title: string};
   srcAudio: {url: string, loop: boolean, autoplay: boolean, title: string};
+  playIcon = 'pause';
+  audioDuration: number;
+  currentPosition: number;
+  restTime: string;
 
-  constructor() { }
+  @ViewChild('audioElement', { static: false }) public audioRef: ElementRef;
+  private audioEl: HTMLMediaElement;
+
+  constructor(public platform: Platform) {
+    this.platform.ready().then(() => this.readAudio());
+  }
 
   ngOnChanges() {
     this.srcAudio = {
@@ -21,4 +32,34 @@ export class AudioPlayerComponent implements OnChanges {
     };
   }
 
+  readAudio() {
+    const audios = this.audioEl;
+    audios.onloadeddata = () => {
+      this.audioDuration = Math.floor(audios.duration);
+    };
+    audios.addEventListener('timeupdate', () => {
+      this.currentPosition = Math.floor(this.audioEl.currentTime);
+      this.convertSec(this.audioDuration - this.currentPosition);
+    });
+  }
+
+  convertSec(seconds: number) {
+    const min = Math.floor((seconds / 60 / 60) * 60);
+    const sec = Math.floor(((seconds / 60 / 60) * 60 - min) * 60);
+    const secD = sec < 10 ? `0${sec}` : sec;
+    this.restTime = `${min}:${secD}`;
+  }
+
+  playPause() {
+    if ( this.playIcon === 'pause' ) {
+      this.playIcon = 'play';
+      this.audioEl.pause();
+    } else {
+      this.playIcon = 'pause';
+      this.audioEl.play();
+    }
+  }
+  public ngAfterViewInit() {
+    this.audioEl = this.audioRef.nativeElement;
+  }
 }
