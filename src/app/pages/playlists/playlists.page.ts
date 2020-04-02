900ad208621a112d1a47e81f47ef2bc4d55043c2
GemmaGarcias4 <gemmagarcias@xpertai.com>
2020-04-02 12:16:08 +0200

    styling views

--- src/app/pages/playlists/playlists.page.ts
+++ src/app/pages/playlists/playlists.page.ts
@@ -21,11 +21,19 @@ export class PlaylistsPage implements OnInit {
   }
 
   groupedArray(items: Item[]) {
-    items.forEach((group: any, idx: number) => {
+    items.map((item) => {
+      return {...item, duration: this.secondsToMinutes(item.duration)};
+    }).forEach((group: any, idx: number) => {
       if (idx % 5 === 0) {
         this.groupedList.push([]);
       }
       this.groupedList[this.groupedList.length - 1].push(group);
     });
+  }
+
+  secondsToMinutes = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const restSecond = seconds - minutes * 60;
+    return minutes + ':' + (+restSecond < 10 ? '0' : '') + restSecond;
   }
 }
